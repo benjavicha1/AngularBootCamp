b31405b1d4c8cd2a4e452efc40d94fe3395e2d2d
benjavicha1 <benjavicha1@gmail.com>
2019-05-04 00:59:33 -0500

    Update

--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,14 +1,18 @@
-interface Point {
-    x: number,
-    y: number
-}
 
-let drawPoint = (point: Point) => {
-    console.log('x: ' + point.x);
-    console.log('y: ' + point.y);
-}
 
-drawPoint({
-    x: 1,
-    y: 2
-});
+
+
+// interface Point {
+//     x: number,
+//     y: number
+// }
+
+// let drawPoint = (point: Point) => {
+//     console.log('x: ' + point.x);
+//     console.log('y: ' + point.y);
+// }
+
+// drawPoint({
+//     x: 1,
+//     y: 2
+// });

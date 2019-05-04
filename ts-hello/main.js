906dbe91079a0d1a59ea339ee0700a7ff889d702
benjavicha1 <benjavicha1@gmail.com>
2019-05-04 13:56:53 -0500

    update

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -1,8 +1,26 @@
-var drawPoint = function (point) {
-    console.log('x: ' + point.x);
-    console.log('y: ' + point.y);
-};
-drawPoint({
-    x: 1,
-    y: 2
-});
+// function log(message) {
+//     console.log(message);
+// }
+// var message = "hello world!";
+// log(message);
+// ------------------------------------- //
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log('Finally: ' + i);
+}
+doSomething();
+// ------------------------------------- //
+// interface Point {
+//     x: number,
+//     y: number
+// }
+// let drawPoint = (point: Point) => {
+//     console.log('x: ' + point.x);
+//     console.log('y: ' + point.y);
+// }
+// drawPoint({
+//     x: 1,
+//     y: 2
+// });
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,6 +1,22 @@
+// function log(message) {
+//     console.log(message);
+// }
 
+// var message = "hello world!";
+// log(message);
+// ------------------------------------- //
 
+// function doSomething() {
+//     // for (var i = 0; i < 5; i++) {
+//     for (let i = 0; i < 5; i++) {
+//         console.log(i);
+//     }
 
+//     console.log('Finally: ' + i);
+// }
+// doSomething();
+
+// ------------------------------------- //
 
 // interface Point {
 //     x: number,

364f0159d374e21de6065f14617141c1b87b8159
benjavicha1 <benjavicha1@gmail.com>
2019-05-18 17:22:44 -0500

    l

--- TypeScriptBasics/app.ts
+++ TypeScriptBasics/app.ts
@@ -9,3 +9,10 @@ if (x === 1) {
 console.log(x);
 // expected output: 1
 
+var inventory = [
+    {name: 'apples', quantity: 2},
+    {name: 'bananas', quantity: 0},
+    {name: 'cherries', quantity: 5}
+];
+
+console.log(Object.keys(inventory[0]))

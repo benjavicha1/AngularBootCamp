b48590df05651eb9e35544baae99f359e2cab435
benjavicha1 <benjavicha1@gmail.com>
2019-05-22 20:58:29 -0500

    commit

---
+++ TypeScriptBasics/types.js
@@ -0,0 +1,24 @@
+// Boolean
+var isThisVideoDone = false;
+// Number
+var myNumber = 4;
+// Text, string
+var myName = "Ben";
+// Template strings
+var greeting = "Hello, " + myName;
+var greeting2 = "Hello, " + myName;
+// Arrays
+var count = [1, 2, 3, 4];
+var count2 = [1, 2, 3, 4];
+// If you don't know the type
+var anything = 4;
+anything = 'some text';
+anything = false;
+// Returning nothing/ type
+function greeter(name) {
+    console.log('Hello, ' + name);
+}
+var greeter2 = function (name) { console.log('Hello, ' + name); };
+var g = greeter2('ben');
+console.log(g); // nothing print out
+// Enums
---
+++ TypeScriptBasics/types.ts
@@ -0,0 +1,38 @@
+// Boolean
+let isThisVideoDone: boolean = false;
+
+
+// Number
+let myNumber: number = 4;
+
+
+// Text, string
+let myName: string = "Ben";
+
+
+// Template strings
+let greeting: string = "Hello, " + myName;
+let greeting2: string = `Hello, ${myName}`;
+
+
+// Arrays
+let count: number[] = [1,2,3,4];
+let count2: Array<number> = [1,2,3,4];
+
+
+// If you don't know the type
+let anything: any = 4;
+anything = 'some text';
+anything = false;
+
+
+// Returning nothing/ type
+function greeter(name: string) : void{
+    console.log('Hello, ' + name);
+}
+let greeter2 = function(name: string): void { console.log('Hello, ' + name); };
+let g = greeter2('ben');
+console.log(g); // output: undefined
+
+
+// Enums

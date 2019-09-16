b01ade4ab80244ea6a1af367d8a99c838f65f08b
Benjavicha Hotrabhavananda <benjavicha1@gmail.com>
2019-09-16 15:08:55 -0500

    Removed tsHello

--- ts-hello/point.ts
+++
@@ -1,9 +0,0 @@
-// Modules
-// ------------------------------------- //
-export class Point {
-    constructor(private _x?: number, private _y?: number) {
-    }
-    draw() {
-        console.log('x: ' + this._x + ', y: ' + this._y);
-    }
-}
